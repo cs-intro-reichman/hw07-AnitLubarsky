6519ad36dc9ddd1d652bbfa27d2382d97d47a24c
Anit Lubarsky <anit.lubarsky@runi.ac.il>
2024-02-07 19:44:49 +0200

    HWO7 FINAL

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -6,10 +6,9 @@ public class HashTagTokenizer {
 
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
-		////printReadDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
-		//existInDictionarTest(hashTag, dictionary);
 	}
+	//To test the readDictionary method
 	public static void printReadDictionary( String fileName) {
 		System.out.print(Arrays.toString(readDictionary(fileName)));
 	}
@@ -22,24 +21,27 @@ public class HashTagTokenizer {
 		}
 		return dictionary;
 	}
+	//To test the exist\inDictionary method
 	public static void existInDictionarTest(String word, String []dictionary) {
 		if(existInDictionary(word, dictionary) == true){
 		System.out.println("yes");
 		} else {
-			System.out.println("NO");
+		System.out.println("NO");
 		}
 	}
+	
 	public static boolean existInDictionary(String word, String []dictionary) {
-		int count = 0;
+		int count = 0; //Counts how many times the character at word is equal to the character at dictionary
 		boolean existInDictionary = false;
 		for( int i = 0; i < dictionary.length; i++){
 			if(word.length() == dictionary[i].length()){
-				count = 0;
+				count = 0; //nullify the count
 				for(int j = 0; j < word.length(); j++){	
 					if(word.charAt(j) == dictionary[i].charAt(j)){
 						count++;
 					}								
 				}
+			
 				if(count == word.length()){
 					existInDictionary = true;
 					break;					
@@ -56,7 +58,8 @@ public class HashTagTokenizer {
             return;
         }
 		int i = 0;
-        int N = hashtag.length();	 
+        int N = hashtag.length();	
+		// Checks if the string is a word. If the loop end and there is no match gets into recursion.
 		for ( i = 1; i <= N; i++){			
 				String hashtagSub = hashtag.substring(0, i);	
 				if(existInDictionary(hashtagSub, dictionary) == true){
@@ -64,6 +67,7 @@ public class HashTagTokenizer {
 				    break;
 				}
 		}
+		//recursion: calling this method on the remaining hashtag string
 		if (i < N ){
 			String remainingHashtag = hashtag.substring(i);	
 			breakHashTag(remainingHashtag,dictionary);
--- SpellChecker.java
+++ SpellChecker.java
@@ -12,10 +12,11 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		String tailStr = str.substring(1);		
-	return tailStr; 
+		return tailStr; 
 	}
+	//To test the levinshtein method
 	public static void levenshteinTest(String word1, String word2) {
-		System.out.println(levenshtein(word1, word2));	
+	System.out.println(levenshtein(word1, word2));	
 	}
 
 	public static int levenshtein(String word1, String word2) {
@@ -48,28 +49,30 @@ public class SpellChecker {
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
+		// To remember the i in the loop in witch the word is in the dictionary 
 		int character1 = -1;
 		int character2 = -1;
 
 		for(int i = 0; i <  dictionary.length; i++){
+			//If the levinshtein distence is grater than the threshold then return the word
 			if (levenshtein(word, dictionary[i]) <= (threshold)){
+				//The word in the dictionary is either equal or longer form the word
 				if(word.length() == dictionary[i].length()){
 					character1 = i;
-					System.out.println("!!!!!");
-
 				} else if(word.length() > dictionary[i].length()) {	
-					System.out.println("****");
 					character2 = i;
 				}					
 			}
 			
 		}
+		//If the word is equal in length to the word in the dictionary than this is the return word
 		if(character1 >= 0){
 				return dictionary[character1];
 			}
-			if(character2 >= 0){
-				return dictionary[character2];
-			}
+		//If the word is shorter than the word in the dictionary than this is the return word 
+		if(character2 >= 0){
+			return dictionary[character2];
+		}
 		return word;
 	}
 }
